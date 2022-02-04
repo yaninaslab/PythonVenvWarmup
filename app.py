7fe1c2e1f81c2abcfb1d05a619d15df662562c43
yaninaslab <yana.belash@gmail.com>
2022-02-03 19:35:01 -0500

    Some minor changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ login_success, user_id = dbi.attempt_login(username, password)
 
 if(login_success == True):
     dbi.list_your_dogs(user_id)
-    print("Authentication success!")
+    #print("Authentication success!")
 
 else:
     print("Sorry, authentication failure")
